54e212375493e0cb98127e1c87573d0c0b8fed14
João D'Amorim <joao.damorim@gmail.com>
2024-05-22 10:50:22 -0300

    Validando Senha.

--- test/core/usuario/UsuarioAnemicoV3.test.ts
+++ test/core/usuario/UsuarioAnemicoV3.test.ts
@@ -1,3 +1,4 @@
+import Erros from '@/constants/Erros'
 import Usuario from '@/core/usuario//UsuarioAnemicoV3'
 
 const usuarioValido = () => new Usuario(
@@ -32,8 +33,20 @@ test('Deve permitir usuario com emai invalido.', () => {
   expect(usuario.getEmail()).toBe('!@Z')
 })
 
-test('Deve permitir usuario com senha invalida.', () => {
+// test('Deve permitir usuario com senha invalida.', () => {
+//   const usuario: Usuario = usuarioValido()
+//   usuario.setSenha('a')
+//   expect(usuario.getSenha()).toBe('a')
+// })
+
+test('Deve lançar erro ao tentar alterar senha com tamanho menor que 6 ccaracteres.', () => {
   const usuario: Usuario = usuarioValido()
-  usuario.setSenha('a')
-  expect(usuario.getSenha()).toBe('a')
+  expect(() => usuario.setSenha('a')).toThrowError(Erros.SENHA_INVALIDA)
+})
+
+test('Deve alterar senha com tamanho maior ou igual a 6 caracteres.', () => {
+  const novaSenhaValida = '123456'
+  const usuario: Usuario = usuarioValido()
+  usuario.setSenha(novaSenhaValida)
+  expect(usuario.getSenha()).toBe(novaSenhaValida)
 })
