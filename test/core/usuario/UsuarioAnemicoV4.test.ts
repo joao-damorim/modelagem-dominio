1231a2d2ca1e6d0417cbc45817c86d90dd95c8df
João D'Amorim <joao.damorim@gmail.com>
2024-05-23 00:42:48 -0300

    Usuário Anêmico #04 (É a versão mais utilizada no Javascript e Typescript)

---
+++ test/core/usuario/UsuarioAnemicoV4.test.ts
@@ -0,0 +1,58 @@
+import Erros from '@/constants/Erros'
+import Usuario from '@/core/usuario//UsuarioAnemicoV4'
+
+const usuarioValido = () => new Usuario(
+  123,
+  'Fulano',
+  'fulano@mail.com',
+  '123456'
+)
+  
+
+test('Deve permitir usuario sem nome.', () => {
+  const usuario: Usuario = usuarioValido()
+  usuario.nome = undefined as any
+  expect(usuario.nome).toBe(undefined)
+})
+
+test('Deve permitir usuario com nome undefined.', () => {
+  const usuario: Usuario = usuarioValido()
+  usuario.nome = undefined as any
+  expect(usuario.nome).toBe(undefined)
+})
+
+test('Deve permitir usuario com id negativo.', () => {
+  const usuario: Usuario = usuarioValido()
+  usuario.id = -123
+  expect(usuario.id).toBe(-123)
+})
+
+test('Deve permitir usuario com emai invalido.', () => {
+  const usuario: Usuario = usuarioValido()
+  usuario.email = '!@Z'
+  expect(usuario.email).toBe(usuario.email)
+})
+
+test('Deve alterar email com email válido.', () => {
+  const usuario: Usuario = usuarioValido()
+  usuario.email = 'fulan@gmail.com.br'
+  expect(usuario.email).toBe(usuario.email)
+})
+
+// test('Deve permitir usuario com senha invalida.', () => {
+//   const usuario: Usuario = usuarioValido()
+//   usuario.setSenha('a')
+//   expect(usuario.getSenha()).toBe('a')
+// })
+
+test('Deve lançar erro ao tentar alterar senha com tamanho menor que 6 ccaracteres.', () => {
+  const usuario: Usuario = usuarioValido()
+  expect(() => usuario.senha = 'a').toThrowError(Erros.SENHA_INVALIDA)
+})
+
+test('Deve alterar senha com tamanho maior ou igual a 6 caracteres.', () => {
+  const novaSenhaValida = '123456'
+  const usuario: Usuario = usuarioValido()
+  usuario.senha = novaSenhaValida
+  expect(usuario.senha).toBe(novaSenhaValida)
+})
