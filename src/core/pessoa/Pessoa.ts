de65f35a81b7dfb0bf5c33ea5f8d9d727ced3945
João D'Amorim <joao.damorim@gmail.com>
2024-06-04 08:46:47 -0300

    Pessoa Cpf

--- src/core/pessoa/Pessoa.ts
+++ src/core/pessoa/Pessoa.ts
@@ -1,15 +1,19 @@
+import Cpf from "../shared/Cpf";
 import Id from "../shared/Id";
 import NomePessoa from "../shared/NomePessoa";
 
 export default class Pessoa {
   readonly id: Id
   readonly nome: NomePessoa
+  readonly cpf: Cpf
 
   constructor(
     nome: string,
+    cpf: string,
     id?: string
   ) {
     this.nome = new NomePessoa(nome)
+    this.cpf = new Cpf(cpf)
     this.id = new Id(id)
   }
 }
--- test/core/pessoa/Pessoa.test.ts
+++ test/core/pessoa/Pessoa.test.ts
@@ -2,11 +2,11 @@ import Erros from "@/constants/Erros"
 import Pessoa from "@/core/pessoa/Pessoa"
 
 test('Deve lançar erro ao tentar criar uma pessoa com nome vazio.', () => {
-  expect(() => new Pessoa('')).toThrowError(Erros.NOME_VAZIO)
+  expect(() => new Pessoa('', '')).toThrowError(Erros.NOME_VAZIO)
 })
 
 test('Deve criar uma pessoa válida.', () => {
-  const pessoa = new Pessoa('Pedro Augusto Soares')
+  const pessoa = new Pessoa('Pedro Augusto Soares', '935.619.760-18')
   expect(pessoa.nome.primeiroNome).toBe('Pedro')
   expect(pessoa.id.novo).toBeTruthy()
 })
