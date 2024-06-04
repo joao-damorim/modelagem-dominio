d019cd3057699f6625e4ea2af95f6e8ee20f719b
João D'Amorim <joao.damorim@gmail.com>
2024-06-03 22:35:31 -0300

    Desafio CPF #2

--- src/core/shared/Cpf.ts
+++ src/core/shared/Cpf.ts
@@ -2,33 +2,31 @@ import Erros from '@/constants/Erros'
 import { isCPF, isCNPJ } from 'validation-br'
 
 export default class Cpf {
-  // readonly valor: string
+  readonly valor: string
 
-  // constructor(valor?: string) {
-  //   this.valor = valor
+  constructor(valor?: string) {
+    this.valor = valor?.replace(/\D/g, '') ?? ''
+    if(!Cpf.isValido(this.valor)) throw new Error(Erros.CPF_INVALIDO)
+  }
 
-  //   if(!isCPF(this.valor)) throw new Error(Erros.CPF_INVALIDO)
-  // }
+  get formatado() {
+    return this.valor.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
+  }
 
-  // get formatado() {
-
-  // }
-
-  // get digitoVerificador() {
-
-  // }
+  get digitoVerificador() {
+    return this.valor.slice(9)
+  }
 
   static isValido(cpf: string): boolean {
     if(!cpf) return false
     const nums = cpf.replace(/\D/g, '').split('')
     if(nums.length !== 11) return false
-    const dv1 = Cpf.validarDV(nums.splice(0,9), nums[9])
-    const dv2 = Cpf.validarDV(nums.splice(1,9), nums[10])
+    const dv1 = Cpf.validarDV(nums.slice(0,9), nums[9], [10, 9, 8, 7, 6, 5, 4, 3, 2])
+    const dv2 = Cpf.validarDV(nums.slice(0,10), nums[10], [11, 10, 9, 8, 7, 6, 5, 4, 3, 2])
     return dv1 && dv2
   }
 
-  private static validarDV(digitos: string[], dvInformado: string): boolean {
-    const fatores = [10,9,8,7,6,5,4,3,2]
+  private static validarDV(digitos: string[], dvInformado: string, fatores: number[]): boolean {
     const total = digitos.reduce((total, digito, i) => {
       return total + (+digito * fatores[i])
     }, 0)
--- test/core/shared/Cpf.test.ts
+++ test/core/shared/Cpf.test.ts
@@ -1,3 +1,4 @@
+import Erros from "@/constants/Erros"
 import Cpf from "@/core/shared/Cpf"
 
 test('Deve retornar cpf inválido para string vazia.', () => {
@@ -12,13 +13,39 @@ test('Deve retornar cpf inválido para string incompleta.', () => {
 })
 
 test('Deve retornar cpf inválido para dv inválido.', () => {
-  expect(Cpf.isValido('123')).toBeFalsy()
-  expect(Cpf.isValido('123456')).toBeFalsy()
-  expect(Cpf.isValido('123123421234')).toBeFalsy()
   expect(Cpf.isValido('123.423.421-00')).toBeFalsy()
 })
 
 test('Deve retornar cpf válido para dv válido.', () => {
   expect(Cpf.isValido('450.163.980-62')).toBeTruthy
   expect(Cpf.isValido('935.619.760-18')).toBeTruthy
+})
+
+test('Deve retornar o dv do cpf.', () => {
+  expect(new Cpf('450.163.980-62').digitoVerificador).toBe('62')
+  expect(new Cpf('935.619.760-18').digitoVerificador).toBe('18')
+})
+
+test('Deve lançar erro para cpf com string vazia.', () => {
+  expect(() => new Cpf('')).toThrowError(Erros.CPF_INVALIDO)
+  expect(() => new Cpf()).toThrowError(Erros.CPF_INVALIDO)
+})
+
+test('Deve lançar erro para cpf com strin incompleta.', () => {
+  expect(() => new Cpf('123')).toThrowError(Erros.CPF_INVALIDO)
+  expect(() => new Cpf('123456')).toThrowError(Erros.CPF_INVALIDO)
+  expect(() => new Cpf('123123421234')).toThrowError(Erros.CPF_INVALIDO)
+  expect(() => new Cpf('123.123.421-23')).toThrowError(Erros.CPF_INVALIDO)
+})
+
+test('Deve lançar erro para cpf com dv inválido.', () => {
+  expect(() => new Cpf('123.423.421-00')).toThrowError(Erros.CPF_INVALIDO)
+})
+
+test('Deve retornar o cpf formatado.', () => {
+  expect(new Cpf('93561976018').formatado).toBe("935.619.760-18")
+})
+
+test('Deve retornar o valor do cpf.', () => {
+  expect(new Cpf('935.619.760-18').valor).toBe("93561976018")
 })
